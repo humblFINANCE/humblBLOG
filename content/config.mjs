78fc5f381539ee7d2a9425998f9957ef1e395ebf
jenningsFantini <jenningsfantini+github@gmail.com>
2024-09-29 17:10:16 -0400

    feat(search): added search

--- content/config.mjs
+++ content/config.mjs
@@ -1,5 +1,33 @@
-
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+export default {
+  title: "humblBLOG",
+  description: "Jennings Fantini's personal blog",
+  author: "Jennings Fantini",
+  logo: "/images/logo.png",
+  // You might want to add a custom favicon
+  // favicon: "/images/favicon.ico",
+  
+  // Additional configuration options
+  navLinks: [
+    { href: "/", name: "Home" },
+    { href: "/blog", name: "Blog" },
+    { href: "/about", name: "About" },
+  ],
+  
+  // Social media links (modified to be an array)
+//   social: [
+//     { name: "twitter", href: "https://x.com/humblFINANCE" },
+//     { name: "github", href: "https://github.com/humblFINANCE/humblBLOG" },
+//     // Add more social media links as needed
+//   ],
+  search: {
+    provider: "algolia",
+    config: {
+      appId: "URO1H3H6JJ",
+      apiKey: "86db8c3cc8cb94e096f1faf8cf220d32",
+      indexName: "",
+    },
+  },
+};
